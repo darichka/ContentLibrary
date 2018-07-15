dc80bb8e6d0635361ecab2674eee4ee832edc4ed
darichka <dariakatyuschenko@gmail.com>
2018-07-16 00:16:43 +0300

    Updated slider and rubrics on index.html

--- js/common.js
+++ js/common.js
@@ -3,10 +3,14 @@ var search = document.getElementById('search');
 search.addEventListener('click', showSearchDialog);
 search.nextElementSibling.querySelector('.close-btn').addEventListener('click', hideSearchDialog);
 
-function showSearchDialog(event){
+function showSearchDialog(){
+    if(search.nextElementSibling.open) {
+        hideSearchDialog();
+        return;
+    }
     search.nextElementSibling.open = true;
 }
-function hideSearchDialog(event){
+function hideSearchDialog(){
     if(search.nextElementSibling.open){
         search.nextElementSibling.open = false;
     }
@@ -14,31 +18,30 @@ function hideSearchDialog(event){
 
 var slider = document.getElementById('slider');
 var imgs = slider.getElementsByTagName('figure');
-var mainSlider = new Slider(slider.children[1]);
+var sliderBox = document.getElementById('slider-box');
+var mainSlider = new Slider(sliderBox);
 slider.firstElementChild.addEventListener('click', mainSlider.slideRight);
 slider.lastElementChild.addEventListener('click', mainSlider.slideLeft);
-// setInterval(mainSlider.showNext, 3000);
+sliderBox.addEventListener('mouseover', pauseInterval);
+sliderBox.addEventListener('mouseout', continueInterval);
+var intervalId = setInterval(mainSlider.slideLeft, 5000);
+startInterval.start = performance.now();
 
-// function Slider(imgs){
-//     this.imgs = imgs;
-//     this.currentImgId = 0;
-//     if(imgs.length) imgs[this.currentImgId].style.display = 'inline-block';
+function startInterval(pause = 0){
+   var duration = 5000 - pause;
+   intervalId = setInterval(mainSlider.slideLeft, duration);
+   startInterval.start = performance.now();
+   console.log(performance.now());
+}
 
-//     var self = this;
+function pauseInterval(){
+    pauseInterval.dur = performance.now() - startInterval.start;
+    clearInterval(intervalId);
+}
 
-//     self.showNext = function(){
-//         self.imgs[self.currentImgId].style.display = 'none';
-//         (self.currentImgId < self.imgs.length-1) ? self.currentImgId++ : self.currentImgId = 0;
-//         self.imgs[self.currentImgId].style.display = 'inline-block';
-//     }
-
-//     self.showPrevious = function(){
-//         self.imgs[self.currentImgId].style.display = 'none';
-//         (self.currentImgId > 0) ? self.currentImgId-- : self.currentImgId = self.imgs.length-1;
-//         self.imgs[self.currentImgId].style.display = 'inline-block';
-//     }
-// }
-
+function continueInterval(){
+    startInterval(pauseInterval.dur);
+}
 
 function Slider(sliderBox){
     this.boxWidth = sliderBox.offsetWidth;
@@ -48,84 +51,145 @@ function Slider(sliderBox){
     this.displayed = 0;
     var self = this;
 
+    initialise();
+
     self.show = function(isLeft){
-        var margin = (self.boxWidth - calcWidth())/(self.currentImgs.length - 1);
-        var i = 0;
+        var margin = calcMargin();
+        self.currentImgs.forEach(function(item, i, arr){
+            delTransition(item);
+            if(isLeft){
+                item.style.left = 100 + '%';
+            }
+            else{
+                item.style.left = -100 + '%';
+            }
+        })
+        if(isLeft){
+            i = 0;
             setTimeout(function slide(){
-                if(isLeft){
-                    self.currentImgs[i].style.left = 100 + '%';
-                }
-                else{
-                    self.currentImgs[i].style.left = -100 + '%';
-                }
-                self.currentImgs[i].style.transition = 'all 1s';
-                self.currentImgs[i].style.left = i*(calcWidth()/self.currentImgs.length + margin) + 'px';
+                addTransition(self.currentImgs[i]);
+                move(self.currentImgs[i], i, margin);
                 if((i+1) < self.currentImgs.length){
                     i++;
                     setTimeout(slide, 200);
                 }
-            }, 100);
+                else startInterval();
+            }, 200); 
+        }
+        else{
+            i = self.currentImgs.length - 1;
+            setTimeout(function slide(){
+                addTransition(self.currentImgs[i]);
+                move(self.currentImgs[i], i, margin);
+                if(i > 0){
+                    i--;
+                    setTimeout(slide, 200);
+                }
+                else startInterval();
+            }, 200); 
+        }
     }
 
     self.hide = function(isLeft){
-        var j = 0;
-        setTimeout(function slide(){
-            if(isLeft){
-                self.previousImgs[j].style.left = -100 + '%';
-                
-            }
-            else{
-                self.previousImgs[j].style.left = 100 + '%';
-                self.previousImgs[j].style.transition = 'none';
-            }
-            if((j+1) < self.previousImgs.length){
-                j++;
-                setTimeout(slide, 200);
-            }
-            else{
-                self.show(isLeft);
-            }
-        }, 200);
+        if(isLeft){
+            var j = 0;
+            setTimeout(function slide(){
+                    self.previousImgs[j].style.left = -100 + '%';
+                    if((j+1) < self.previousImgs.length){
+                        j++;
+                         setTimeout(slide, 200);
+                    }
+                    else{
+                         self.show(isLeft);
+                     }
+                }, 200);
+             }
+        else{
+            var j = self.previousImgs.length - 1;
+            setTimeout(function slide(){
+                    self.previousImgs[j].style.left = 100 + '%';
+                    if(j > 0){
+                        j--;
+                         setTimeout(slide, 200);
+                    }
+                    else{
+                         self.show(isLeft);
+                     }
+                }, 200);
+             }
     }
-
-    for(var i = 0; i < self.imgs.length; i++){
-        if ((calcWidth() + self.imgs[i].offsetWidth) > self.boxWidth) break;
-        self.currentImgs.push(imgs[i]);
-    }
-    self.show(true);
 
     self.slideLeft = function(){
+        clearInterval(intervalId);
         self.previousImgs = self.currentImgs;
         self.hide(true);
         self.currentImgs = [];
         var i;
-        console.log(self.displayed + self.previousImgs.length);
         ((self.displayed + self.previousImgs.length) < self.imgs.length) ? i = self.displayed += self.previousImgs.length : i = self.displayed += self.previousImgs.length - self.imgs.length;
         while((calcWidth() + self.imgs[i].offsetWidth) < self.boxWidth){
             self.currentImgs.push(imgs[i]);
             i++;
             (i < self.imgs.length) ? i = i : i = 0;
         }
-}
-
-self.slideRight = function(){
-    self.previousImgs = self.currentImgs;
-    self.hide(false);
-    self.currentImgs = [];
-    var i;
-   console.log(self.displayed - self.previousImgs.length);
-    (self.displayed - self.previousImgs.length > 0) ? i = self.displayed -= self.previousImgs.length : i = self.displayed = self.imgs.length - (self.displayed + 1) ;
-    while((calcWidth() + self.imgs[i].offsetWidth) < self.boxWidth){
-        self.currentImgs.push(imgs[i]);
-        i--;
-        (i > 0) ? i = i : i = self.imgs.length - 1;
     }
-}
+
+    self.slideRight = function(){
+        clearInterval(intervalId);
+        self.previousImgs = self.currentImgs;
+        self.hide(false);
+        self.currentImgs = [];
+        var i;
+        (self.displayed - self.previousImgs.length > 0) ? i = self.displayed -= self.previousImgs.length : i = self.displayed = self.imgs.length - self.displayed - self.previousImgs.length;
+        while((calcWidth() + self.imgs[i].offsetWidth) < self.boxWidth){
+            self.currentImgs.push(imgs[i]);
+            i++;
+            (i < self.imgs.length) ? i = i : i = 0;
+        }
+    }
 
     function calcWidth(){
         return self.currentImgs.reduce(function(width, current){
             return width + current.offsetWidth;
         }, 0);
+    }
+
+    function calcMargin(){
+    	var margin;
+    	if(self.currentImgs.length <= 1)
+    	{
+    		margin = (self.boxWidth - calcWidth())/2;
+    		return margin;
+    	}
+        margin = (self.boxWidth - calcWidth())/(self.currentImgs.length - 1);
+        return margin;
+    }
+
+    function move(img, i, margin){
+    	if(self.currentImgs.length < 2 ){
+    		img.style.left = margin + 'px';
+    		return;
+    	}
+        img.style.left = i*(calcWidth()/self.currentImgs.length + margin) + 'px';
+    }
+
+    function initialise(){
+        for(var i = 0; i < self.imgs.length; i++){
+            if ((calcWidth() + self.imgs[i].offsetWidth) > self.boxWidth) break;
+            self.currentImgs.push(imgs[i]);
+        }
+        var margin = calcMargin();
+        self.currentImgs.forEach(function(item, i, arr){
+            move(item, i, margin);
+            addTransition(item);
+        })
+    }
+
+    function delTransition(item){
+        item.style.transition = 'none';
+    }
+
+    function addTransition(item){
+        item.style.transition = 'all 0.5s';
     }
 }
 
